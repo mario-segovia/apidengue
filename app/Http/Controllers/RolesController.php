4493a6f9526b64d6d1be3081fc4514134ce36d3b
mario-segovia <mariodavid.segoviatroche@gmail.com>
2020-09-09 18:03:02 -0400

    ABM Roles agregado

---
+++ app/Http/Controllers/RolesController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Roles;
+use Illuminate\Http\Request;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Roles::all();
+        return $roles;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $roles = Roles::create($request->all());
+      return $roles;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function show( $id)
+    {
+      $roles = Roles::find($id);
+      return $roles;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $roles = Roles::find($id);
+      $roles->update($request->all());
+      return $roles;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy( $id)
+    {
+      $roles = Roles::find($id);
+      $roles->delete();
+      return $roles;
+    }
+}
